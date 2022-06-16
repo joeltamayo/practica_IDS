ee7449d583d628a38e96577a839f3420d057419c
Joel Tamayo <jueltamayo25@gmail.com>
2022-06-15 20:46:02 -0500

    Commit inicial con la funcion main

---
+++ programa.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Musica
+{
+    char genero[50], artista[50], album[50];
+};
+
+void introducirDatos(struct Musica *);
+void mostrarDatos(struct Musica);
+
+int main()
+{
+    struct Musica metadatos;
+
+    // introducirDatos(&metadatos);
+    // mostrarDatos(metadatos);
+
+    printf("\n\n\n\n");
+    system("pause");
+    return 0;
+}
