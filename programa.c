17b01cd1c019c9b9c9e224cb68864b3853499a66
mavs7347 <107590368+mavs7347@users.noreply.github.com>
2022-06-15 23:13:28 -0500

    Funcion introducir datos

--- programa.c
+++ programa.c
@@ -6,17 +6,32 @@ struct Musica
     char genero[50], artista[50], album[50];
 };
 
-//void introducirDatos(struct Musica *);
+void introducirDatos(struct Musica *);
 //void mostrarDatos(struct Musica);
 
 int main()
 {
     struct Musica metadatos;
 
-    //introducirDatos(&metadatos);
+    introducirDatos(&metadatos);
     // mostrarDatos(metadatos);
 
     printf("\n\n\n\n");
     system("pause");
     return 0;
+}
+
+void introducirDatos(struct Musica *meta)
+{
+	printf("\nIntroduzca el nombre del album: ");
+	fflush(stdin);
+	gets(meta->album);
+	
+	printf("\nIntroduzca el nombre del artista: ");
+	fflush(stdin);
+	gets(meta->artista);
+	
+	printf("\nIntroduzca el genero del album: ");
+	fflush(stdin);
+	gets(meta->genero);
 }
